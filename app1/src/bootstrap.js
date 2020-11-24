5affa84bdd8d2ab427244ed01ad752103fa82bba
nicole-dsouza <nicole.dsouza@mail.com>
2020-11-24 19:13:49 +0400

    working version without ssr

--- app1/src/bootstrap.js
+++ app1/src/bootstrap.js
@@ -7,6 +7,7 @@ function App() {
   const [count, setCount] = useState(0);
   return (
     <>
+      <p>I'm from app1</p>
       <h1>Hello from React component</h1>
       <React.Suspense fallback='Loading Counter...'>
         <Counter
--- app2/src/components/Counter.js
+++ app2/src/components/Counter.js
@@ -3,6 +3,7 @@ import React from 'react';
 function Counter(props) {
   return (
     <>
+      <p>I'm from app2!</p>
       <p>Count: {props.count}</p>
       <button onClick={props.onIncrement}>Increment</button>
       <button onClick={props.onDecrement}>Decrement</button>
